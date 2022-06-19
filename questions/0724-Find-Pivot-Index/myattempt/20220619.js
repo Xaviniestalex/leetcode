3e2f2d4b40e017b2d5fa4fb04d0ee609b5e0768c
Xaviniesta <88670766+Xaviniestalex@users.noreply.github.com>
2022-06-20 00:00:23 +0800

    0724 Completion

--- questions/0724-Find-Pivot-Index/myattempt/20220619.js
+++ questions/0724-Find-Pivot-Index/myattempt/20220619.js
@@ -6,7 +6,7 @@ var pivotIndex = function (nums) {
     let leftSum = 0;
     let rightSum = nums.reduce((partialSum, a) => partialSum + a, 0) - nums[0];
     if (leftSum === rightSum) return 0;
-    for (i = 1; i < nums.length - 1; i++) {
+    for (i = 1; i < nums.length; i++) {
         leftSum += nums[i - 1];
         rightSum -= nums[i];
         if (leftSum === rightSum) return i;
@@ -24,8 +24,10 @@ var pivotIndex = function (nums) {
 const test1 = pivotIndex([1, 7, 3, 6, 5, 6]); // 3
 const test2 = pivotIndex([1, 2, 3]); // -1
 const test3 = pivotIndex([2, 1, -1]); // 0
+const test4 = pivotIndex([-1, -1, 0, 1, 1, 0]); // 5
 
 const printOutput = (test) => console.log(test)
 printOutput(test1);
 printOutput(test2);
 printOutput(test3);
+printOutput(test4);
