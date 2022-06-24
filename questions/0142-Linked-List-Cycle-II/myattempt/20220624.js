f1f2b802b59a09ec29eac0e4ab4fd9d8e0beda5b
Xaviniesta <88670766+Xaviniestalex@users.noreply.github.com>
2022-06-24 19:23:55 +0800

    0142 setup

---
+++ questions/0142-Linked-List-Cycle-II/myattempt/20220624.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+
+};
+
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+/**
+ * Complexity Analysis
+ * - Time Complexity: IDK
+ * - Space Complexity: IDK
+ */
+
+// tests
+const test1 = detectCycle(); // 
+const test2 = detectCycle(); // 
+const test3 = detectCycle(); // 
+
+const printOutput = (test) => console.log(test)
+printOutput(test1);
+printOutput(test2);
+printOutput(test3);
